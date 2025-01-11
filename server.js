f1d0be20438c94708d22ef404f6ab35a71c8a475
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-01-11 19:14:06 +0530

    final commit

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ import authRouts from './routs/authRoute.js';
 import categoryRoutes from './routs/categoryRoutes.js'; // Correct import
 import productRoutes from './routs/productRoutes.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(cors());
@@ -23,8 +27,13 @@ app.use(morgan('dev'));
 app.use('/api/v1/auth', authRouts);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/products', productRoutes);
+app.use(express.static(path.join(__dirname, './client/build')))
 
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+})
 app.listen(port, () => console.log(`Server is running in ${process.env.DEV_MODE} mode on port ${port}!`.bgGreen.blue));
