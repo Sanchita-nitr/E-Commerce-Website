13fa63158a756a466ff30b7251e0d59e09a80e63
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-01-15 02:01:27 +0530

    final commit

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -1,19 +1,17 @@
-
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 export default function useCategory() {
     const [categories, setCategories] = useState([]);
 
-    //get categories 
     const getCategories = async () => {
         try {
-            const { data } = await axios.get('/api/v1/category/getall-category');
+            const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/getall-category`);
             setCategories(data?.category);
         } catch (error) {
-            console.error(error);
+            console.error('Error fetching categories:', error);
         }
-    }
+    };
 
     useEffect(() => {
         getCategories();
