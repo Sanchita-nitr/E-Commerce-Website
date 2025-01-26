2766f7b22ad3a8704ae5e3476a0737267795701c
Sanchita Priyadarshinee <146603307+Sanchita-nitr@users.noreply.github.com>
2025-01-26 23:03:56 +0530

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -14,25 +14,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
     <SearchProvider>
-   
-        <CartProvider>
+      <CartProvider>
         <WishlistProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
-          </WishlistProvider>
-        </CartProvider>
-      
-
-
+        </WishlistProvider>
+      </CartProvider>
     </SearchProvider>
-
   </AuthProvider>
-
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
