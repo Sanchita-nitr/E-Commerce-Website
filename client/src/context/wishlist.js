7503b5f4ec154d2ee8d475b69a3ac774bec12a50
Sanchita Priyadarshinee <146603307+Sanchita-nitr@users.noreply.github.com>
2025-01-26 22:44:39 +0530

    wishlist.js

--- client/src/context/wishlist.js
+++ client/src/context/wishlist.js
@@ -1,37 +1,4 @@
-// import { createContext, useContext, useEffect, useState } from 'react';
 
-// // Create a context for the cart
-// const WishlistContext = createContext();
-
-// // CartProvider component
-// const WishlistProvider = ({ children }) => {
-//     // Initialize cart state inside the CartProvider component
-//     const [wishlist, setWishlist] = useState([]);
-
-//     // Load cart items from localStorage only once on component mount
-//     useEffect(() => {
-//         const existingWishlistItem = localStorage.getItem('wishlist');
-//         if (existingWishlistItem) {
-//             setWishlist(JSON.parse(existingWishlistItem));
-//         }
-//     }, []);
-
-//     // Update localStorage whenever the cart changes
-//     useEffect(() => {
-//         localStorage.setItem('wishlist', JSON.stringify(wishlist));
-//     }, [wishlist]);
-
-//     return (
-//         <WishlistContext.Provider value={[wishlist, setWishlist]}>
-//             {children}
-//         </WishlistContext.Provider>
-//     );
-// };
-
-// // Custom hook to use the cart context
-// const useWishlist = () => useContext(WishlistContext);
-
-// export { WishlistProvider, useWishlist };
 import { createContext, useContext, useEffect, useState } from 'react';
 
 // Create a context for the wishlist
