179af3255bf7065efe4aefd503f2ac588bbfde1d
Sanchita Priyadarshinee <146603307+Sanchita-nitr@users.noreply.github.com>
2025-01-26 22:36:00 +0530

    SearchInput.js

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -33,11 +33,11 @@ const SearchInput = () => {
                         placeholder="Search..."
                         value={values.keyword}
                         onChange={(e) => setValues({ ...values, keyword: e.target.value })}
-                        className="w-full pl-4 pr-12 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+                        className="w-full pl-4 pr-12 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800 focus:border-blue-500"
                     />
                     <button
                         type="submit"
-                        className="absolute inset-y-0 right-0 flex items-center justify-center w-10 h-full bg-green-500 text-white rounded-r-lg hover:bg-green-600"
+                        className="absolute inset-y-0 right-0 flex items-center justify-center w-10 h-full bg-blue-900 text-white rounded-r-lg hover:bg-blue-950"
                     >
                         <IoSearch />
                     </button>
